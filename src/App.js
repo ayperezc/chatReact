b4c1d02fd34860a8a0fcfd954f2a8a1aa2764780
Yarid Perez <ayperezc@unal.edu.co>
2019-10-22 12:47:28 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import ApolloClient from 'apollo-boost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'react-chat-widget/lib/styles.css';
+
+//EJECUTAR npm install --save react-chat-widget
+
+
+class App extends Component {
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
+  }
+  handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    addResponseMessage("Mensaje receptor");
+  }
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() { 
+    return (  
+      <div className="App">
+        <Widget title="Nombre Remitente" handleNewUserMessage={this.handleNewUserMessage}/>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
